49375df7036c8baa0e1eecab2d978bf2eb42b076
AlaaMostapha <alaamoustapha21@gmail.com>
2021-06-19 15:35:35 +0200

    add layout

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import "bootstrap/dist/css/bootstrap.css";
+import "../styles/globals.css";
+import Layout from "../components/Layout";
+import { Provider } from "react-redux";
+import store from "../store";
+function MyApp({ Component, props }) {
+  return (
+    <Provider store={store}>
+      <Layout>
+        <Component {...props} />
+      </Layout>
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
